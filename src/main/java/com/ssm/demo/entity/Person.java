e08fac6d70e7360e372ca5cf042a57132c0604da
ysx19950313 <329090804@qq.com>
2020-01-18 21:05:10 +0800

    SsmDemo

---
+++ src/main/java/com/ssm/demo/entity/Person.java
@@ -0,0 +1,19 @@
+package com.ssm.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private long id;
+    private String name;
+    private String email;
+    private int status;
+
+    //getter  setter
+
+    @Override
+    public String toString(){
+        return this.getId()+"---"+this.getName()+"---"+
+                this.getEmail()+"---"+this.getStatus();
+    }
+}
